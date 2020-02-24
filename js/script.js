8521688ec315a0f3d9a24b9021072eff3a0a1b69
dikushan00 <dikushant@gmail.com>
2020-02-24 20:00:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+var slidebarOn = document.querySelector(".slide");
+var sliderbar = document.querySelector(".slidebar");
+
+function doIt(){
+    slidebar.style.display = "block";
+}
+
+slidebarOn.addEventListener("click", doIt);
